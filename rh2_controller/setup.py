dcc907ce6b5e08687d6b7a2b23f67d53c77f4c43
LiTang <LiTang0617@outlook.com>
2025-09-08 20:23:39 +0800

    feat: update

--- rh2_controller/setup.py
+++ rh2_controller/setup.py
@@ -1,16 +1,18 @@
 from setuptools import setup, find_packages
+import os
+from glob import glob
 
 package_name = 'rh2_controller'
 
 setup(
     name=package_name,
     version='1.0.0',
-    packages=find_packages(),
+    packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name + '/launch', ['launch/rh2_controller.launch.py']),
+        ('share/' + package_name + '/launch', glob('launch/*.launch.py')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -24,4 +26,6 @@ setup(
             'rh2_dual_hand_node = rh2_controller.rh2_node:main',
         ],
     },
+    package_dir={'': '.'},  # 指定包的根目录
+    python_requires='>=3.8',  # 指定Python版本要求
 )
